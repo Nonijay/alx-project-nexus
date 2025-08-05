e7c795e38f2e4f704136f24f23091d07dd04f0ff
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-08-05 16:39:39 -0500

    add API integration for fetching product data

--- next-shop/app/page.tsx
+++ next-shop/app/page.tsx
@@ -1,11 +1,41 @@
 import Image from "next/image";
+import {stripe} from "@/lib/stripe"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Carousel from "@/components/carousel";
 
-export default function Home() {
-  return (
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 5,
+  });
+
+  return(
     <div>
-      <h1>Welcome to Next Shop</h1>
-      <p>Your next best shopping Market</p>
+      <section>
+        <div>
+          <div>
+            <h2>Welcome to Next Shop</h2>
+            <p>Your next best shop at cheaper prices</p>
+            <Button asChild variant="default">
+
+              <Link href="/products">
+                Browse All Products
+              </Link>
+                
+            </Button>
+          </div>
+          <Image 
+          alt="Banner-image" 
+          width={250} height={250} 
+          src={products.data[4].images[0]}>
+
+          </Image>
+        </div>
+      </section>
+      <section>
+        <Carousel/>
+      </section>
     </div>
-    
-  );
+  )
 }
