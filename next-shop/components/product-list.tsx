20a88d08de5e776f60c4ea6b9846b0088f483ed4
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-08-12 12:36:31 -0500

    feat: implement simple search & product filtering

--- next-shop/components/product-list.tsx
+++ next-shop/components/product-list.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import React from 'react'
 import Stripe from 'stripe'
 import ProductCard from './product-card'
+import {useState} from 'react'
 
 
 interface Props {
@@ -9,17 +12,33 @@ interface Props {
 
 
 const ProductList = ({ products }: Props ) => {
+    const [searchTerm,setSearchTerm] = useState<string>("")
+
+    const filteredProduct = products.filter((product) => {
+        const term = searchTerm.toLowerCase()
+        const nameMatch = product.name.toLowerCase().includes(term)
+        const descriptionMatch = product.description
+            ? product.description.toLowerCase().includes(term)
+            : false;
+
+        return nameMatch || descriptionMatch ;    
+    });
+
+
+
   return (
     <div>
         <div className = "mb-6 flex justify-center">
             <input 
-                type='text' 
+                type='text'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)} 
                 placeholder='Search products....'
-                className='w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' 
+                className='w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-orange-400' 
             />
         </div>
         <ul className='mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-            {products.map((product, key) => {
+            {filteredProduct.map((product, key) => {
             return (
                 <li key={key}>
                     <ProductCard product = {product} />
